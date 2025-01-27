07938936c17fb7f551ba447d73f36a885339ae90
Eusebiu Vlad Hidis <Eusebiu.Hidis@zucchetti.com>
2025-01-27 21:48:27 +0200

    routing before update to angular v 18

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -2,22 +2,24 @@ import { Component, inject } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { NgIf } from '@angular/common';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, NgIf],
+  imports: [FormsModule, NgIf, RouterLink, RouterLinkActive],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
   authService=inject(AuthService);
+  router=inject(Router)
   model:any={};
   login(){
     console.log(this.model);
     this.authService.login(this.model).subscribe({
-      next:response=>{
-        console.log(response);
+      next:_=>{
+        void this.router.navigateByUrl('/members')
       },
       error:error=>console.log(error)
     })
@@ -25,6 +27,7 @@ export class NavComponent {
   logout(){
     console.log("user has been logged out");
     this.authService.logout();
+    this.router.navigateByUrl('/');
     
   }
 }
